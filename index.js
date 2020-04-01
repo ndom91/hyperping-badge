80ab362490ee5e89709b97d8b353bc4317d64f53
ndo@ndo3 <yo@ni.co.de>
2020-04-01 18:58:22 +0200

    update: MVP

--- index.js
+++ index.js
@@ -3,42 +3,57 @@ addEventListener("fetch", event => {
 });
 
 const generate = async request => {
-  let badge2 = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="20">
-    <linearGradient id="smooth" x2="0" y2="100%">
-          <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
-          <stop offset="1" stop-opacity=".1"/>
-    </linearGradient>
-
-    <mask id="round">
-      <rect width="100" height="20" rx="4" fill="#fff"/>
-    </mask>
-
-    <g mask="url(#round)">
-      <rect width="30" height="20" fill="#555"/>
-      <rect x="30" width="30" height="20" fill="#67b246"/>
-      <rect width="100" height="20" fill="url(#smooth)"/>
-    </g>
-
-    <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="12">
-      <text x="11" y="15" fill="#010101" fill-opacity=".3">Test</text>
-      <text x="11" y="14">Test</text>
-      <text x="35" y="15" fill="#010101" fill-opacity=".3">Lol</text>
-      <text x="35" y="14">Lol</text>
-    </g>
-  </svg> 
+  let badge = `
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="80" height="20">
+      <linearGradient id="b" x2="0" y2="100%">
+        <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+        <stop offset="1" stop-opacity=".1"/>
+      </linearGradient>
+      <clipPath id="a">
+        <rect width="80" height="20" rx="3" fill="#fff"/>
+      </clipPath>
+      <g clip-path="url(#a)">
+        <path fill="#555" d="M0 0h35v20H0z"/>
+        <path fill="#007ec6" d="M35 0h45v20H35z"/>
+        <path fill="url(#b)" d="M0 0h80v20H0z"/>
+      </g>
+      <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110"> 
+      <text x="185" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="250">
+        [LEFT]
+      </text>
+      <text x="185" y="140" transform="scale(.1)" textLength="250">
+        [LEFT]
+      </text>
+      <text x="565" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="350">
+        {RIGHT}
+      </text>
+      <text x="565" y="140" transform="scale(.1)" textLength="350">
+        {RIGHT}
+      </text>
+      </g> 
+    </svg>
   `;
 
-  return new Response(badge2, {
+  const url = new URL(request.url);
+  const urlParams = new URLSearchParams(url.search);
+  const left = urlParams.get("left");
+  console.log("left", left);
+  const right = urlParams.get("right");
+  console.log("right", right);
+  const color = urlParams.get("color");
+  console.log("color", color);
+
+  badge = badge.replace(/[LEFT]/g, left);
+  badge = badge.replace(/{RIGHT}/g, right);
+  badge = badge.replace(/{COLOR}/g, color);
+
+  return new Response(badge, {
     status: 200,
     headers: { "Content-Type": "image/svg+xml" }
   });
 };
 
 async function handleRequest(request) {
-  console.log("H", request.headers);
-  console.log("U", request.url);
-
   if (request.method === "GET") {
     // response = new Response("Hello worker!", { status: 200 });
     response = await generate(request);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   target: "webworker",
   entry: "./dist/main.js",
-  mode: "production",
-  context: __dirname,
-  node: {
-    fs: "empty"
-  }
+  mode: "production"
 };
